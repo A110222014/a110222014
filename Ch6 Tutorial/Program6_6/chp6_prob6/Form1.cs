e094d59f4a7b4dec67ccde5b58b9e68bf11bc134
A110222014 <a110222014@mail.shu.edu.tw>
2024-05-20 16:45:28 +0800

    1130520

--- Ch6 Tutorial/Program6_6/chp6_prob6/Form1.cs
+++ Ch6 Tutorial/Program6_6/chp6_prob6/Form1.cs
@@ -11,20 +11,56 @@ namespace chp6_prob6
 {
     public partial class Form1 : Form
     {
-        const decimal stayPerDay = 550.00m;
+        const decimal stayPerDay = 5500.00m;
         public Form1()
         {
             InitializeComponent();
         }
 
-       
 
+
+        private decimal CalcStayCharges(int day)
+        {
+            return stayPerDay * day;
+        }
+
+        private decimal CalcMissCharges(decimal foodBill, decimal spaBill, decimal carRental, decimal medicaBill)
+            {
+            return (foodBill + spaBill + carRental + medicaBill);
+        }
+
+        private decimal CalcTotalCharges(decimal stayBill, decimal miscellaneous)
+            {
+            return (stayBill + miscellaneous);
+        }
+        private bool IsInputValid(ref int days, ref decimal fb, ref decimal sb, ref decimal cb, ref decimal nb)
+
+        {
+            if(int.TryParse(textBox1.Text, out days) && decimal.TryParse(textBox2.Text, out fb) && decimal.TryParse(textBox3.Text, out sb)&& decimal.TryParse(textBox4.Text, out cb)&& decimal.TryParse(textBox5.Text, out nb))
+            {
+                return true;
+            }
+            
+                else
+            {
+                MessageBox.Show("資料格式錯誤");
+                return false;
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            int days;
-            decimal fb, sb, cb, mb;
+            int days = 0;
+            decimal fb = 0, sb = 0, cb = 0, mb = 0;
             decimal stayCharge, miscCharge;
             decimal totalCharge;
+
+            if(IsInputValid (ref days, ref fb, ref sb, ref cb, ref mb))
+            {
+                stayCharge = CalcStayCharges(days);
+                miscCharge = CalcMissCharges(fb, sb, cb, mb);
+                totalCharge = CalcTotalCharges(stayCharge, miscCharge);
+                MessageBox.Show("Total Bill:" + totalCharge.ToString("c"));
+            }
          
 
         }
@@ -32,6 +68,11 @@ namespace chp6_prob6
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
